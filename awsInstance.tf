5721f8d6e3ccb2bfa26ee707c43e0c2fa2c3db72
jherbage <jasonherbage@googlemail.com>
2017-02-22 11:43:53 +0000

    removed keys from public git

---
+++ awsInstance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "terraformConfiguredInstance" {
+  ami           = "ami-cbfcd2b8"
+  instance_type = "t2.micro"
+}
